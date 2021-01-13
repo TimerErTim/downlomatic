bcde4f4d942ba66eb2780f5a7d06e9148bc0c841
TimerErTim <timerertim@gmail.com>
2021-01-13 13:00:32 +0100

    Create ReadableConsumerByteChannel for fast downloads

---
+++ src/main/java/org/example/downloader/utils/ReadableConsumerByteChannel.java
@@ -0,0 +1,44 @@
+package org.example.downloader.utils;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ReadableByteChannel;
+import java.util.function.IntConsumer;
+
+public class ReadableConsumerByteChannel implements ReadableByteChannel {
+    private final ReadableByteChannel byteChannel;
+    private final IntConsumer onRead;
+
+    private int totalByteRead;
+
+    public ReadableConsumerByteChannel(ReadableByteChannel byteChannel, IntConsumer onRead) {
+        this.byteChannel = byteChannel;
+        this.onRead = onRead;
+        totalByteRead = 0;
+    }
+
+    @Override
+    public int read(ByteBuffer byteBuffer) throws IOException {
+        int nRead = byteChannel.read(byteBuffer);
+        notifyBytesRead(nRead);
+        return nRead;
+    }
+
+    protected void notifyBytesRead(int nRead) {
+        if (nRead <= 0) {
+            return;
+        }
+        totalByteRead += nRead;
+        onRead.accept(totalByteRead);
+    }
+
+    @Override
+    public boolean isOpen() {
+        return byteChannel.isOpen();
+    }
+
+    @Override
+    public void close() throws IOException {
+        byteChannel.close();
+    }
+}
