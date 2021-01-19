a4f894a0239a45381319a57480d8a637921c9ca8
TimerErTim <timerertim@gmail.com>
2021-01-19 17:13:58 +0100

    Create basic layout of AnimeToast implementation

---
+++ src/main/java/org/example/downloader/animetoast/AnimeToastPage.java
@@ -0,0 +1,13 @@
+package org.example.downloader.animetoast;
+
+import org.example.downloader.core.Series;
+import org.example.downloader.core.SeriesProvider;
+
+public class AnimeToastPage implements SeriesProvider {
+
+
+    @Override
+    public Series provideSeries() {
+        return null;
+    }
+}
---
+++ src/main/java/org/example/downloader/animetoast/AnimeToastSeries.java
@@ -0,0 +1,6 @@
+package org.example.downloader.animetoast;
+
+import org.example.downloader.core.Series;
+
+public class AnimeToastSeries extends Series {
+}
