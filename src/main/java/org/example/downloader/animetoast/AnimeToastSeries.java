765f21a4c4c6b90ef67a0272c34f14758388a5d9
TimerErTim <timerertim@gmail.com>
2021-01-20 11:40:13 +0100

    Override Series methods in AnimeToastSeries, implementation pending

--- src/main/java/org/example/downloader/animetoast/AnimeToastSeries.java
+++ src/main/java/org/example/downloader/animetoast/AnimeToastSeries.java
@@ -1,6 +1,34 @@
 package org.example.downloader.animetoast;
 
+import org.example.downloader.core.Downloader;
 import org.example.downloader.core.Series;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Set;
+
 public class AnimeToastSeries extends Series {
+    //TODO: Implement
+    protected AnimeToastSeries(URL seriesURL) throws MalformedURLException {
+        super(seriesURL);
+    }
+
+    protected AnimeToastSeries(String seriesURLString) throws MalformedURLException {
+        super(seriesURLString);
+    }
+
+    @Override
+    protected Set<? extends Downloader> generateEpisodeDownloaders() {
+        return null;
+    }
+
+    @Override
+    public boolean needsJavaScript() {
+        return false;
+    }
+
+    @Override
+    public String getInvalidSeriesMessage() {
+        return null;
+    }
 }
