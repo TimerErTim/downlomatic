1cfbfe69506d8ffa76b3dc375b95db740940218c
TimerErTim <timerertim@gmail.com>
2021-01-20 12:23:10 +0100

    Create HentaiPlaySeriesTest

---
+++ src/test/java/org/example/downloader/hentaiplayer/HentaiPlaySeriesTest.java
@@ -0,0 +1,32 @@
+package org.example.downloader.hentaiplayer;
+
+import org.example.downloader.core.Download;
+import org.example.downloader.core.Downloader;
+import org.example.downloader.core.WebScrapers;
+import org.example.downloader.hentaiplay.HentaiPlaySeries;
+
+import java.net.MalformedURLException;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class HentaiPlaySeriesTest {
+    public static void main(String[] args) throws MalformedURLException {
+        WebScrapers.initialize();
+        Set<Download> downloadPool = new LinkedHashSet<>();
+        HentaiPlaySeries series = new HentaiPlaySeries("https://hentaiplay.net/episode-list/dropout/");
+        series.fillEpisodeDownloaders();
+        int i = 1;
+        for (Downloader downloader : series) {
+            Download download = downloader.generateVideoDownload("/home/timerertim/Downloads/Test" + i++ + ".mp4");
+            synchronized (downloadPool) {
+                downloadPool.add(download);
+            }
+            download.startParallel((success) -> {
+                synchronized (downloadPool) {
+                    downloadPool.remove(download);
+                }
+                System.out.println(download.toString() + " is finished");
+            });
+        }
+    }
+}
