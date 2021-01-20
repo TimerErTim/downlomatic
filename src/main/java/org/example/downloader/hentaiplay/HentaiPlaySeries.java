29fdd5071a8be7e9b9c921f02b522e6e67deecbc
TimerErTim <timerertim@gmail.com>
2021-01-20 11:34:58 +0100

    Implement HentaiPlaySeries

---
+++ src/main/java/org/example/downloader/hentaiplay/HentaiPlaySeries.java
@@ -0,0 +1,68 @@
+package org.example.downloader.hentaiplay;
+
+import org.example.downloader.core.Series;
+import org.example.downloader.core.WebScrapers;
+import org.example.downloader.utils.JSoupDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
+
+public class HentaiPlaySeries extends Series {
+
+    /**
+     * Creates a HentaiPlaySeries Object from the URL.
+     *
+     * @param seriesURL the URL
+     * @throws MalformedURLException the the exception that is thrown if the URL is no valid HentaiPlay site
+     */
+    protected HentaiPlaySeries(URL seriesURL) throws MalformedURLException {
+        super(seriesURL);
+        if (!(HentaiPlayPage.isHentaiPlayPage(seriesURL) && seriesURL.toString().contains("/episode-list/"))) {
+            throw new MalformedURLException("URL " + seriesURL + " is no URL leading to a hentaiplay.net episode list");
+        }
+    }
+
+    /**
+     * Creates a HentaiPlaySeries Object from the String.
+     *
+     * @param seriesURLString the String representing URL to HentaiPlay site
+     * @throws MalformedURLException the exception that is thrown if the URL is no HentaiPlay site
+     */
+    protected HentaiPlaySeries(String seriesURLString) throws MalformedURLException {
+        super(seriesURLString);
+    }
+
+    @Override
+    protected Set<HentaiPlayDownloader> generateEpisodeDownloaders() {
+        Set<HentaiPlayDownloader> downloaders = new LinkedHashSet<>();
+        JSoupDriver driver = WebScrapers.noJavaScript();
+
+        driver.get(seriesURL.toString());
+        List<WebElement> elements = driver.findElements(By.className("category-episodes"));
+        for (WebElement element : elements) {
+            try {
+                HentaiPlayDownloader downloader = new HentaiPlayDownloader(element.findElement(By.className("clip-link")).getAttribute("href"));
+                downloaders.add(downloader);
+            } catch (MalformedURLException ignored) {
+
+            }
+        }
+
+        return downloaders;
+    }
+
+    @Override
+    public boolean needsJavaScript() {
+        return false;
+    }
+
+    @Override
+    public String getInvalidSeriesMessage() {
+        return "URL \"" + seriesURL + "\" is no valid series on hentaiplay.net";
+    }
+}
