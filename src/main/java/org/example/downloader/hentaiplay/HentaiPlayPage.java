19cbd130e1a18ae2103395bdc6e718609d7962fa
TimerErTim <timerertim@gmail.com>
2021-01-23 00:21:09 +0100

    ="= Create method in HentaiPlayPage to get every Series from the WebPage

--- src/main/java/org/example/downloader/hentaiplay/HentaiPlayPage.java
+++ src/main/java/org/example/downloader/hentaiplay/HentaiPlayPage.java
@@ -3,17 +3,60 @@ package org.example.downloader.hentaiplay;
 import org.example.downloader.core.PageManager;
 import org.example.downloader.core.Series;
 import org.example.downloader.core.SeriesProvider;
+import org.example.downloader.core.WebScrapers;
+import org.example.downloader.utils.JSoupDriver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.LinkedHashSet;
+import java.util.List;
+import java.util.Set;
 
 public class HentaiPlayPage extends PageManager implements SeriesProvider {
+    private static Set<Series> allSeries;
+
     public static boolean isHentaiPlayPage(URL url) {
         return url.toString().startsWith("https://hentaiplay.net/");
     }
 
+    /**
+     * Returns every Series available on that website.
+     * <p>
+     * The {@code Set} of {@link Series} only contains "uninitialized"
+     * {@code Series} objects.
+     *
+     * @return all Series objects fetchable from the webpage
+     */
+    public static Set<Series> generateAllSeries() {
+        if (allSeries == null) {
+            allSeries = new LinkedHashSet<>();
+
+            JSoupDriver driver = WebScrapers.noJavaScript();
+            driver.get("https://hentaiplay.net/hentai-index/");
+            List<WebElement> seriesWrappers = driver.findElements(By.className("serieslist-content"));
+            for (WebElement wrapper : seriesWrappers) {
+                WebElement link = wrapper.findElement(By.tagName("a"));
+                if (link != null) {
+                    Series series;
+                    try {
+                        series = new HentaiPlaySeries(link.getAttribute("href"));
+                    } catch (MalformedURLException e) {
+                        series = null;
+                    }
+                    if (series != null) {
+                        allSeries.add(series);
+                    }
+                }
+            }
+        }
+
+        return allSeries;
+    }
+
     @Override
-    public Series provideSeries(String episodeFormat) throws MalformedURLException {
+    public Series provideSeries() throws MalformedURLException {
         return null;
     }
 }
