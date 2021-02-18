f14048b4fd4d33abfa4f6415eb3aea9eb1353d2f
TimerErTim <timerertim@gmail.com>
2021-02-18 15:16:54 +0100

    Extract EpisodeFormatBuilder from EpisodeFormat

--- src/main/java/org/example/downloader/core/format/EpisodeFormat.java
+++ src/main/java/org/example/downloader/core/format/EpisodeFormat.java
@@ -8,8 +8,8 @@ import java.util.regex.Pattern;
 public class EpisodeFormat {
     private final List<Identifier> identifiers;
 
-    private EpisodeFormat(String seriesName, String seasonNumber, String episodeNumber, String episodeName,
-                          String language, String translationType) {
+    EpisodeFormat(String seriesName, String seasonNumber, String episodeNumber, String episodeName,
+                  String language, String translationType) {
         this.identifiers = new LinkedList<>();
         identifiers.add(new Identifier("S", seriesName));
         identifiers.add(new Identifier("s", seasonNumber));
@@ -88,118 +88,6 @@ public class EpisodeFormat {
         }
 
         return segment;
-    }
-
-    /**
-     * Generates a non mutable EpisodeFormat given specific parameters.
-     */
-    public static class EpisodeFormatGenerator {
-        private String episodeName, seriesName;
-        private String episodeNumber, seasonNumber;
-        private String language, translationType;
-
-        /**
-         * Generates a EpisodeFormatGenerator. It's only
-         * purpose is to generate a non mutable EpisodeFormat.
-         *
-         * @param seriesName    the series' name
-         * @param seasonNumber  the number of the season
-         * @param episodeNumber the number of the episode
-         * @param episodeName   the name of the episode
-         */
-        public EpisodeFormatGenerator(String seriesName, String seasonNumber, String episodeNumber, String episodeName,
-                                      String language, String translationType) {
-            this.episodeName = episodeName;
-            this.seriesName = seriesName;
-            this.episodeNumber = episodeNumber;
-            this.seasonNumber = seasonNumber;
-            this.language = language;
-            this.translationType = translationType;
-        }
-
-        /**
-         * Generates a EpisodeFormatGenerator. It's only
-         * purpose is to generate a non mutable EpisodeFormat.
-         */
-        public EpisodeFormatGenerator() {
-            this(null, null, null, null, null, null);
-        }
-
-        /**
-         * Sets the series name of the EpisodeFormat.
-         *
-         * @param seriesName the series' name
-         * @return this Object for easy chaining of expressions.
-         */
-        public EpisodeFormatGenerator setSeriesName(String seriesName) {
-            this.seriesName = seriesName;
-            return this;
-        }
-
-        /**
-         * Sets the season number of the EpisodeFormat.
-         *
-         * @param seasonNumber the number of the season
-         * @return this Object for easy chaining of expressions.
-         */
-        public EpisodeFormatGenerator setSeasonNumber(String seasonNumber) {
-            this.seasonNumber = seasonNumber;
-            return this;
-        }
-
-        /**
-         * Sets the episode number of the EpisodeFormat.
-         *
-         * @param episodeNumber the number of the episode
-         * @return this Object for easy chaining of expressions.
-         */
-        public EpisodeFormatGenerator setEpisodeNumber(String episodeNumber) {
-            this.episodeNumber = episodeNumber;
-            return this;
-        }
-
-        /**
-         * Sets the episode name of the EpisodeFormat.
-         *
-         * @param episodeName the name of the episode
-         * @return this Object for easy chaining of expressions.
-         */
-        public EpisodeFormatGenerator setEpisodeName(String episodeName) {
-            this.episodeName = episodeName;
-            return this;
-        }
-
-        /**
-         * Sets the translation type of the EpisodeFormat.
-         *
-         * @param translationType the method of displaying the audience language (sub, dub, raw/none/OV, etc.)
-         * @return this Object for easy chaining of expressions.
-         */
-        public EpisodeFormatGenerator setTranslationType(String translationType) {
-            this.translationType = translationType;
-            return this;
-        }
-
-        /**
-         * Sets the language of the EpisodeFormat.
-         *
-         * @param language the audience language of the episode
-         * @return this Object for easy chaining of expressions.
-         */
-        public EpisodeFormatGenerator setLanguage(String language) {
-            this.language = language;
-            return this;
-        }
-
-        /**
-         * Generates a non mutable EpisodeFormat with
-         * the configured entries.
-         *
-         * @return a final EpisodeFormat
-         */
-        public EpisodeFormat generate() {
-            return new EpisodeFormat(seriesName, seasonNumber, episodeNumber, episodeName, language, translationType);
-        }
     }
 
     private static class Identifier {
