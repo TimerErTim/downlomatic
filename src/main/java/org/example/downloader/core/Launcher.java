bd61cb65c6ce6a8abdacfafb70564be7e6723a92
TimerErTim <timerertim@gmail.com>
2021-02-22 14:40:49 +0100

    Make use of Utils class in Launcher

--- src/main/java/org/example/downloader/core/Launcher.java
+++ src/main/java/org/example/downloader/core/Launcher.java
@@ -4,7 +4,8 @@ import org.example.downloader.core.download.CollectiveDownloadBuilder;
 import org.example.downloader.core.framework.Downloader;
 import org.example.downloader.core.framework.Series;
 import org.example.downloader.graphics.GUI;
-import org.example.downloader.hentaiplay.HentaiPlayPage;
+import org.example.downloader.pages.hentaiplay.HentaiPlayPage;
+import org.example.downloader.utils.Utils;
 import org.example.downloader.utils.WebScrapers;
 
 import java.net.MalformedURLException;
@@ -18,7 +19,7 @@ public class Launcher {
     //TODO: hentaigasm.com -> HD Hentais
     //TODO: Adjust and implement (right now just a prototype)
     public static void main(String[] args) throws MalformedURLException {
-        WebScrapers.initialize();
+        Utils.initializeSetup();
         if (!Arrays.asList(args).contains("nogui")) {
             GUI.start(args);
         } else {
