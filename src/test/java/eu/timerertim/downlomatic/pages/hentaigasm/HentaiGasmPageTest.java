7af3e02bfde23dac35976f239c09de85cbf8a537
TimerErTim <timerertim@gmail.com>
2021-03-18 13:12:39 +0100

    Create Test for HentaiGasmPage fix

---
+++ src/test/java/eu/timerertim/downlomatic/pages/hentaigasm/HentaiGasmPageTest.java
@@ -0,0 +1,19 @@
+package eu.timerertim.downlomatic.pages.hentaigasm;
+
+import eu.timerertim.downlomatic.core.framework.Host;
+import eu.timerertim.downlomatic.core.framework.Series;
+import eu.timerertim.downlomatic.utils.Utils;
+
+import java.net.MalformedURLException;
+import java.util.Set;
+
+public class HentaiGasmPageTest {
+    public static void main(String... args) throws MalformedURLException {
+        Utils.initializeSetup();
+        Host host = new HentaiGasmPage();
+        Set<Series> series = host.getSeries();
+        for (Series serie : series) {
+            System.out.println(serie.getName());
+        }
+    }
+}
