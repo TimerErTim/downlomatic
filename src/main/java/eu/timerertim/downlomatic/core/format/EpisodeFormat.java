569ac3601ccaea5bf81ed55756164f8504341761
TimerErTim <timerertim@gmail.com>
2021-03-24 12:35:47 +0100

    Overhaul EpisodeFormat to pass all tests and work as intended

--- src/main/java/eu/timerertim/downlomatic/core/format/EpisodeFormat.java
+++ src/main/java/eu/timerertim/downlomatic/core/format/EpisodeFormat.java
@@ -1,10 +1,10 @@
 package eu.timerertim.downlomatic.core.format;
 
+import org.jetbrains.annotations.NotNull;
+
 import javax.annotation.Nonnull;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
+import java.io.File;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
@@ -17,21 +17,25 @@ import java.util.stream.Collectors;
  * described in that methods JavaDoc documentation.
  */
 public class EpisodeFormat {
-    public final static String DESCRIPTION = "These are the types of identifiers:\n" +
-            "\"/S\": Means the name of the series\n" +
-            "\"/s\": Means the number of the season\n" +
-            "\"/E\": Means the name of the episode\n" +
-            "\"/e\": Means the number of the episode\n" +
-            "\"/L\": Means the main target audience language of the episode/movie\n" +
-            "\"/T\": Means the type of translation\n" +
-            "\"/[\" or \"/]\": Covered in the later paragraph\n" +
-            "\"//\": Means literally \"/\"\n" +
-            "If those identifiers are found in the given expression, they are replaced by their respective meaning. Every other part of the expression is seen as literal and returned as is.\n" +
-            "Characters, which can not be used in the name of a file will be removed. This are mainly Windows specific illegals.\n" +
-            "If there is an identifier in the expression, which there is no value for, the identifier by default is left out and simply removed. You can adjust this behavior by using \"/[\" and \"/]\". You can put these identifiers around a segment of the expression. The segment will then only be displayed if every identifier inside that segment can successfully be filled in. If there are missing closing/opening identifier, the identifiers causing the problem are ignored.\n" +
-            "In each of these segments you can make use of \"inverted identifiers\". Inverted identifiers make the segment they are in only visible if there exists no value for them. They can be created by putting a \"!\" directly after \"/\" of each identifier. For example \"/[S/sE/e/]/[/!sEpisode /E]\".";
+    public static final String WIKI_URL = "https://github.com/TimerErTim/downlomatic/wiki/Formatting";
 
     private final List<Identifier> identifiers;
+    private static final List<Literal> literals = Arrays.asList(
+            new Literal("//", File.separator)
+    );
+    private static final List<Illegal> illegals = Arrays.asList(
+            new Illegal("/"),
+            new Illegal("\\"),
+            new Illegal("["),
+            new Illegal("]"),
+            new Illegal(":"),
+            new Illegal("*"),
+            new Illegal("?"),
+            new Illegal("\""),
+            new Illegal("<"),
+            new Illegal(">"),
+            new Illegal("|")
+    );
 
     EpisodeFormat(String seriesName, String seasonNumber, String episodeNumber, String episodeName,
                   String language, String translationType) {
@@ -42,7 +46,48 @@ public class EpisodeFormat {
         identifiers.add(new Identifier("e", episodeNumber));
         identifiers.add(new Identifier("L", language));
         identifiers.add(new Identifier("T", translationType));
-        identifiers.add(new Literal("//", "/"));
+
+        identifiers.addAll(literals);
+        identifiers.addAll(illegals);
+    }
+
+    private static List<String> findSegments(String input) {
+        List<String> segments = new LinkedList<>();
+        Stack<String> stack = new Stack<>();
+        int startIndex = 0;
+        int endIndex;
+
+        int openingIndex;
+        int closingIndex;
+
+        int index = 0;
+        // Checking for opening and closing brackets with a stack
+        for (; ; index++) {
+            openingIndex = input.indexOf("/[", index);
+            closingIndex = input.indexOf("/]", index);
+
+            if (openingIndex == -1 && closingIndex == -1) {
+                break;
+            } else if (openingIndex < closingIndex && openingIndex != -1) {
+                if (stack.isEmpty()) {
+                    startIndex = openingIndex;
+                }
+                stack.push("/[");
+                index = openingIndex;
+            } else if (closingIndex < openingIndex || openingIndex == -1) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                    if (stack.isEmpty()) {
+                        // If the stack is empty, the last bracket must have been reached, so we can save a segment
+                        endIndex = closingIndex + 1;
+                        segments.add(input.substring(startIndex, endIndex + 1));
+                    }
+                }
+                index = closingIndex;
+            }
+        }
+
+        return segments;
     }
 
     /**
@@ -87,57 +132,84 @@ public class EpisodeFormat {
      * @return a formatted String representation of an episode
      */
     public String format(String expression) {
-        final String regex = "\\/\\[(?<content>[^\\[\\]]*?)\\/\\]";
-        final Pattern pattern = Pattern.compile(regex, Pattern.MULTILINE);
+        List<String> subSegments;
 
-        Matcher matcher = pattern.matcher(expression);
-        while (matcher.find()) {
-            expression = matcher.replaceAll((match) -> formatSegment(match.group(1)));
-            matcher = pattern.matcher(expression);
-        }
+        // If the expression is a segment
+        if ((subSegments = findSegments(expression)).size() == 1 && subSegments.get(0).equals(expression)) {
+            expression = expression.substring(2, expression.length() - 2);
+            subSegments = findSegments(expression);
 
-        String temp = expression.replaceAll("(\\/\\[|\\/\\])", "");
-        temp = replaceSimultaneous(temp);
+            // Remove inner segments
+            List<Literal> replacements = new LinkedList<>();
+            for (String s : subSegments) {
+                replacements.add(new Illegal(s));
+            }
+            String temp = replaceSimultaneous(expression, replacements, true);
 
-        return temp.replaceAll("[\\\\:*?\"<>|]", "");
-    }
-
-    private String formatSegment(String segment) {
-        for (Identifier identifier : identifiers) {
-            boolean hasValue = identifier.hasValue();
-            if (segment.contains(identifier.getIdentifier()) && !hasValue || segment.contains(identifier.getNegativeIdentifier()) && hasValue) {
-                return "";
+            // Return empty string according to segment logic
+            for (Identifier identifier : identifiers) {
+                boolean hasValue = identifier.hasValue();
+                if (temp.contains(identifier.getIdentifier()) && !hasValue || temp.contains(identifier.getNegativeIdentifier()) && hasValue) {
+                    return "";
+                }
             }
         }
 
-        return replaceSimultaneous(segment);
+        // Calculate replacements for inner segments
+        List<Literal> replacements = new LinkedList<>();
+        for (String s : subSegments) {
+            replacements.add(new Literal(s, format(s)));
+        }
+
+        // Return filled in expression
+        return replaceSimultaneous(expression, replacements);
     }
 
-    private String replaceSimultaneous(String input) {
-        String regexp = identifiers.stream().map(Identifier::getReplaceRegex).collect(Collectors.joining("|"));
-        Map<String, String> replacements = new HashMap<>();
-        identifiers.forEach(identifier -> {
-            replacements.put(identifier.getIdentifier(), identifier.getValue());
-            replacements.put(identifier.getNegativeIdentifier(), identifier.getValue());
-        });
+    private String replaceSimultaneous(String input, List<? extends Identifier> extra) {
+        return replaceSimultaneous(input, extra, false);
+    }
 
-        StringBuffer sb = new StringBuffer();
-        Pattern p = Pattern.compile(regexp);
-        Matcher m = p.matcher(input);
-
-        while (m.find()) {
-            m.appendReplacement(sb, replacements.get(m.group()));
+    private String replaceSimultaneous(String input, List<? extends Identifier> extra, boolean overwrite) {
+        // Create replacement pool
+        List<Identifier> identifiers = new LinkedList<>(extra);
+        if (!overwrite) {
+            identifiers.addAll(this.identifiers);
         }
-        m.appendTail(sb);
+        // Create replacement regular expression
+        String regexp = identifiers.stream().map(Identifier::getReplaceRegex).collect(Collectors.joining("|"));
+        if (regexp.isEmpty()) {
+            // Return input as is if there's nothing to replace
+            return input;
+        } else {
+            // Create replacement map out of replacement pool
+            Map<String, String> replacements = new HashMap<>();
+            identifiers.forEach(identifier -> {
+                replacements.put(identifier.getIdentifier(), identifier.getValue());
+                replacements.put(identifier.getNegativeIdentifier(), "");
+            });
 
-        return sb.toString();
+            // Replace everything "simultaneously"
+            StringBuffer sb = new StringBuffer();
+            Pattern p = Pattern.compile(regexp);
+            Matcher m = p.matcher(input);
+
+            while (m.find()) {
+                m.appendReplacement(sb, replacements.get(m.group()));
+            }
+            m.appendTail(sb);
+
+            return sb.toString();
+        }
     }
 
     private static class Identifier {
         private final String identifier, negativeIdentifier, value;
 
-        private Identifier(String key, String value) {
-            this("/" + key, "/!" + key, value);
+        public Identifier(String key, String value) {
+            this("/" + key, "/!" + key,
+                    value != null ? value.replaceAll(
+                            illegals.stream().map(Identifier::getReplaceRegex).collect(Collectors.joining("|")),
+                            "") : null);
         }
 
         private Identifier(String identifier, String negativeIdentifier, String value) {
@@ -169,7 +241,7 @@ public class EpisodeFormat {
     }
 
     private static class Literal extends Identifier {
-        private Literal(@Nonnull String key, @Nonnull String value) {
+        public Literal(@Nonnull String key, @Nonnull String value) {
             super(key, String.valueOf(Character.MIN_VALUE), value);
         }
 
@@ -181,6 +253,12 @@ public class EpisodeFormat {
         @Override
         protected String getReplaceRegex() {
             return "(" + Pattern.quote(getIdentifier()) + ")";
+        }
+    }
+
+    private static class Illegal extends Literal {
+        public Illegal(@NotNull String key) {
+            super(key, "");
         }
     }
 }
