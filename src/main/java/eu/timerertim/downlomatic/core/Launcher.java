73cb0d29e649c59e9394f368a0a342a3c5bbbaad
TimerErTim <timerertim@gmail.com>
2021-03-30 22:37:27 +0200

    Refactor Launcher with new arguments parsing feature

--- src/main/java/eu/timerertim/downlomatic/core/Launcher.java
+++ src/main/java/eu/timerertim/downlomatic/core/Launcher.java
@@ -1,127 +1,98 @@
 package eu.timerertim.downlomatic.core;
 
+import eu.timerertim.downlomatic.console.Arguments;
+import eu.timerertim.downlomatic.console.ConsoleParsedArguments;
+import eu.timerertim.downlomatic.console.ConsoleUtils;
 import eu.timerertim.downlomatic.core.download.CollectiveDownloadBuilder;
 import eu.timerertim.downlomatic.core.framework.Host;
 import eu.timerertim.downlomatic.graphics.GUI;
 import eu.timerertim.downlomatic.pages.Hosts;
 import eu.timerertim.downlomatic.utils.logging.Log;
-import org.apache.commons.cli.CommandLine;
-import org.apache.commons.cli.Option;
 import org.apache.commons.cli.ParseException;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.Arrays;
-import java.util.LinkedList;
-import java.util.List;
 
-import static eu.timerertim.downlomatic.utils.Utils.*;
+import static eu.timerertim.downlomatic.utils.Utils.initializeSetup;
+import static eu.timerertim.downlomatic.utils.Utils.onExit;
 
 public class Launcher {
-    public final static Option HELP = new Option(null, "help", false, "shows this");
-    public final static Option FILE_LOGGING = new Option(null, "no-file-logging", false, "enables logging to file");
-    public final static Option NSFW = new Option("x", "nsfw", false, "display NSFW hosts in GUI\nif you want to show GUI you need pass only this or no argument");
-    public final static Option DESTINATION_DIRECTORY = new Option("d", "destination", true, "the download destination folder");
-    public final static Option HOST = new Option("h", "host", true, "the host to download from:");
-    public final static Option SERIES = new Option("s", "series", true, "the full URL to a series");
-    public final static Option DOWNLOADER = new Option("b", "download", true, "the full URL to a single video");
-    public final static Option ALL = new Option("a", "all", false, "download every single video from the host");
-    public final static Option MAX_DOWNLOADS = new Option("m", "max-threads", true, "the maximum amount of downloads being executed at the same time");
-    public final static Option DOWNLOAD_FORMAT = new Option("f", "format", true, "formats every single downloaded video according to the guidelines shown " +
-            "at the end of this output");
-    public final static Option SUBDIR_FORMAT = new Option(null, "subdir-format", true, "controls subdirectory creation according to the guidelines shown at the end " +
-            "of this output");
-
     public static void main(String... args) {
-        List<String> arguments = new LinkedList<>(Arrays.asList(args));
-
-        if (!arguments.remove("--" + FILE_LOGGING.getLongOpt())) {
-            Log.setFileLogging(true);
-        }
-
-        CommandLine cmd = null;
-        ParseException ex = null;
         try {
-            cmd = parseOptions(args);
-        } catch (ParseException e) {
-            ex = e;
-        }
+            ConsoleParsedArguments parsedArguments = ConsoleUtils.parseArgs(args);
 
-        if (cmd == null) {
-
-            if (arguments.contains("--" + HELP.getLongOpt())) {
-                printHelp();
-                System.exit(0);
-            } else if (arguments.isEmpty() || arguments.get(0).equals("-" + NSFW.getOpt()) || arguments.get(0).equals("--" + NSFW.getLongOpt())) {
-                try {
-                    if (arguments.isEmpty()) GUI.start();
-                    else GUI.start(NSFW.getLongOpt());
-                    System.exit(0);
-                } catch (RuntimeException e) {
-                    System.out.println("GUI experienced problems. Please refer to following help page as alternative:\n");
-                }
-            } else {
-                System.out.println(ex.getMessage() + "\n");
+            int requiredSizeForGUI = 2;
+            if (!parsedArguments.hasArgument(Arguments.NO_FILE_LOGGING)) {
+                Log.setFileLogging(true);
+                requiredSizeForGUI--;
             }
 
-            printHelp();
-            System.exit(1);
-        } else {
-            launchCLI(cmd);
+            if (parsedArguments.hasArgument(Arguments.HELP)) {
+                ConsoleUtils.printHelp();
+                System.exit(0);
+            }
+
+            if (!parsedArguments.hasRequiredArguments()) {
+                if (parsedArguments.hasArgument(Arguments.NSFW) && parsedArguments.getSize() == requiredSizeForGUI) {
+                    GUI.start(Arguments.NSFW.getOption().getLongOpt());
+                } else if (parsedArguments.getSize() == requiredSizeForGUI - 1) {
+                    GUI.start();
+                } else {
+                    showErrorHelpMessage(parsedArguments.getMissingArgumentMessage());
+                }
+            } else {
+                launchCLI(parsedArguments);
+            }
+        } catch (ParseException e) {
+            showErrorHelpMessage(e.getMessage());
         }
     }
 
-    private static void launchCLI(CommandLine cmd) {
+    private static void launchCLI(ConsoleParsedArguments args) {
         CollectiveDownloadBuilder builder = null;
         Host host = null;
         Integer max = null;
         String downloadFormat = null, subdirFormat = null;
 
         try {
-            host = Hosts.valueOf(cmd.getOptionValue(HOST.getOpt()).toUpperCase()).getHost();
+            host = Hosts.valueOf(args.get(Arguments.HOST)).getHost();
         } catch (IllegalArgumentException e) {
-            System.out.println("Illegal argument: host has to be a valid type\n");
-            printHelp();
-            System.exit(1);
+            showErrorHelpMessage("Illegal argument: host has to be a valid type");
         }
-        if (cmd.hasOption(MAX_DOWNLOADS.getOpt())) {
+
+        if (args.hasArgument(Arguments.MAX_DOWNLOADS)) {
             try {
-                max = Integer.parseInt(cmd.getOptionValue(MAX_DOWNLOADS.getOpt()));
+                max = Integer.parseInt(args.get(Arguments.HOST));
             } catch (IllegalArgumentException e) {
-                System.out.println("Illegal argument: max downloads has to be a number");
-                printHelp();
-                System.exit(1);
+                showErrorHelpMessage("Illegal argument: max downloads has to be a number");
             }
         }
-        if (cmd.hasOption(DOWNLOAD_FORMAT.getOpt())) {
-            downloadFormat = cmd.getOptionValue(DOWNLOAD_FORMAT.getOpt());
+
+        if (args.hasArgument(Arguments.DOWNLOAD_FORMAT)) {
+            downloadFormat = args.get(Arguments.DOWNLOAD_FORMAT);
         }
-        if (cmd.hasOption(SUBDIR_FORMAT.getLongOpt())) {
-            subdirFormat = cmd.getOptionValue(SUBDIR_FORMAT.getLongOpt());
+        if (args.hasArgument(Arguments.SUBDIR_FORMAT)) {
+            subdirFormat = args.get(Arguments.SUBDIR_FORMAT);
         }
 
         initializeSetup();
 
         try {
-            String destinationPath = cmd.getOptionValue(DESTINATION_DIRECTORY.getOpt());
+            String destinationPath = args.get(Arguments.DESTINATION_DIRECTORY);
             String url;
-            if (cmd.hasOption(ALL.getOpt())) {
+            if (args.hasArgument(Arguments.ALL)) {
                 builder = new CollectiveDownloadBuilder(destinationPath, host.getSeries());
-            } else if ((url = cmd.getOptionValue(SERIES.getOpt())) != null) {
+            } else if ((url = args.get(Arguments.SERIES)) != null) {
                 builder = new CollectiveDownloadBuilder(destinationPath, host.getSeries(new URL(url)));
-            } else if ((url = cmd.getOptionValue(DOWNLOADER.getOpt())) != null) {
+            } else if ((url = args.get(Arguments.DOWNLOADER)) != null) {
                 builder = new CollectiveDownloadBuilder(destinationPath, host.getDownloader(new URL(url)));
             } else {
                 onExit();
-                System.out.println("Missing argument: -a | -s | -d\n");
-                printHelp();
-                System.exit(1);
+                showErrorHelpMessage("Missing argument: -a | -s | -d\n");
             }
         } catch (MalformedURLException e) {
             onExit();
-            System.out.println("Illegal argument: " + e.getMessage() + "\n");
-            printHelp();
-            System.exit(1);
+            showErrorHelpMessage("Illegal argument: " + e.getMessage());
         }
 
         if (max != null) {
@@ -138,5 +109,13 @@ public class Launcher {
             onExit();
             System.exit(0);
         });
+
+        builder.execute();
+    }
+
+    private static void showErrorHelpMessage(String errorMessage) {
+        System.out.println(errorMessage + "\n");
+        ConsoleUtils.printHelp();
+        System.exit(1);
     }
 }
