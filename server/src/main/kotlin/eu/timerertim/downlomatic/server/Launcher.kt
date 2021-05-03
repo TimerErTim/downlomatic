864b56b8e8ca31417cab3d614c0cb335dbbc031a
TimerErTim <timerertim@gmail.com>
2021-05-03 19:13:26 +0200

    Add simple argument processing

--- server/src/main/kotlin/eu/timerertim/downlomatic/server/Launcher.kt
+++ server/src/main/kotlin/eu/timerertim/downlomatic/server/Launcher.kt
@@ -1,5 +1,13 @@
 package eu.timerertim.downlomatic.server
 
-fun main() {
+import eu.timerertim.downlomatic.console.ConsoleUtils
+import eu.timerertim.downlomatic.console.ParsedArguments
+
+fun main(args: Array<String>) {
+    val args = ConsoleUtils.parseArgs(*args)
+    processArgs(args)
+}
+
+fun processArgs(arguments: ParsedArguments) {
 
 }
