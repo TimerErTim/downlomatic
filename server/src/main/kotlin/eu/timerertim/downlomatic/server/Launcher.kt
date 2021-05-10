bd7f13050432bb7b382d418d9808b42202037f85
TimerErTim <timerertim@gmail.com>
2021-05-10 22:44:40 +0200

    Add exception and exit handling to Launcher

--- server/src/main/kotlin/eu/timerertim/downlomatic/server/Launcher.kt
+++ server/src/main/kotlin/eu/timerertim/downlomatic/server/Launcher.kt
@@ -3,16 +3,44 @@ package eu.timerertim.downlomatic.server
 import eu.timerertim.downlomatic.console.ConsoleUtils
 import eu.timerertim.downlomatic.console.ParsedArguments
 import eu.timerertim.downlomatic.console.ServerArguments
+import eu.timerertim.downlomatic.utils.ServerUtils
+import eu.timerertim.downlomatic.utils.logging.Level
+import eu.timerertim.downlomatic.utils.logging.Log
 import kotlin.system.exitProcess
 
 fun main(args: Array<String>) {
+    @Suppress("NAME_SHADOWING")
     val args = ConsoleUtils.parseArgs(*args)
     processArgs(args)
+    exitProcess(0)
 }
 
-fun processArgs(arguments: ParsedArguments) {
+private fun processArgs(arguments: ParsedArguments) {
     if (arguments.hasArgument(ServerArguments.HELP)) {
         ConsoleUtils.printHelp()
-        exitProcess(0)
+    } else {
+        if (arguments.hasRequiredArguments()) {
+            ServerUtils.setup()
+
+            // Further config
+            if (arguments.hasArgument(ServerArguments.VERBOSE)) {
+                Log.consoleVerbosity = Level.ALL
+            }
+
+            // Start server, catch and handle exceptions
+            try {
+                startServer()
+                ServerUtils.exit(0)
+            } catch (exception: Exception) {
+                Log.f("A fatal error broke the execution", exception)
+                ServerUtils.exit(3)
+            }
+        } else {
+            arguments.missingArgumentMessage?.let { ConsoleUtils.showErrorHelpMessage(it) }
+        }
     }
+}
+
+private fun startServer() {
+    //TODO: Implement server main functionality
 }
