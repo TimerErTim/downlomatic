8f0715db712db731515fc7634baf0194b62ed67a
TimerErTim <timerertim@gmail.com>
2021-05-10 22:38:56 +0200

    Add Verbose to ServerArguments

--- server/src/main/kotlin/eu/timerertim/downlomatic/console/ServerArguments.kt
+++ server/src/main/kotlin/eu/timerertim/downlomatic/console/ServerArguments.kt
@@ -6,7 +6,8 @@ import org.apache.commons.cli.Option
  * This enum stores every possible argument passable via CLI on the server program.
  */
 enum class ServerArguments(override val option: Option, override val isHidden: Boolean = false) : Argument {
-    HELP(Option(null, "help", false, "shows this"));
+    HELP(Option(null, "help", false, "shows this")),
+    VERBOSE(Option("verbose", "v", false, "enables all levels of log messages"), true);
 
     constructor(option: Option, isHidden: Boolean, setup: Option.() -> Unit) : this(option, isHidden) {
         setup(this.option)
