07a95ccb986502943bb0c24ad41756215bdedf56
TimerErTim <timerertim@gmail.com>
2021-05-10 22:41:38 +0200

    Add ServerUtils

---
+++ server/src/main/kotlin/eu/timerertim/downlomatic/utils/ServerUtils.kt
@@ -0,0 +1,51 @@
+package eu.timerertim.downlomatic.utils
+
+import com.mongodb.MongoClientException
+import eu.timerertim.downlomatic.utils.logging.Level
+import eu.timerertim.downlomatic.utils.logging.Log
+
+/**
+ * Provides some neat helper functions for the server.
+ */
+object ServerUtils {
+
+    /**
+     * Sets up the server to allow successful execution.
+     */
+    @JvmStatic
+    fun setup() {
+        // Invoke neutral Utils setup
+        Utils.setup()
+
+
+        // Setup logging
+        Log.fileLogging = true
+        Log.consoleVerbosity = Level.WARN
+
+        // Setup database connection
+        try {
+            try {
+                DownlomaticDatabase.testConnection()
+            } catch (exception: ExceptionInInitializerError) {
+                throw exception.cause!!
+            }
+        } catch (exception: MongoClientException) {
+            Log.f(
+                "An error occurred while attempting to connect to the local MongoDB. Make sure the mongodb is" +
+                        "running and has authorization deactivated!", exception
+            )
+            exit(2)
+        }
+    }
+
+    /**
+     * Closes and cleans system resources and prepares the program to exit. The given [errorCode] is returned to the OS
+     * (defaults to 0).
+     */
+    @JvmStatic
+    @JvmOverloads
+    fun exit(errorCode: Int = 0) {
+
+        Utils.exit(errorCode)
+    }
+}
