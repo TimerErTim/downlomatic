9d2f82d3aa7e27a1c072233185d57eeeb5f8644a
TimerErTim <timerertim@gmail.com>
2021-05-10 14:02:37 +0200

    Overhaul Metadata structure

--- shared/src/main/kotlin/eu/timerertim/downlomatic/core/meta/Metadata.kt
+++ shared/src/main/kotlin/eu/timerertim/downlomatic/core/meta/Metadata.kt
@@ -1,9 +1,15 @@
 package eu.timerertim.downlomatic.core.meta
 
+import java.time.LocalDateTime
+
 /**
  * This class contains all metadata information about a video.
  *
- * It contains a list of [Tag]s and a [VideoDetails] object, describing exactly what video this [Metadata]
- * belongs to.
+ * It contains information such as size, type, date of last edit and so on.
  */
-data class Metadata(val details: VideoDetails, val tags: List<Tag>)
+data class Metadata(
+    val size: Long = 0,
+    val fileType: String = "",
+    val httpType: String? = null,
+    val lastModified: LocalDateTime? = null
+)
