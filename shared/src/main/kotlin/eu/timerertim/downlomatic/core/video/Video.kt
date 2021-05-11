2b3a317b7ee85424e17e464ec1f2167e84fc07fb
TimerErTim <timerertim@gmail.com>
2021-05-11 22:31:00 +0200

    Add KDoc to Video.kt

--- shared/src/main/kotlin/eu/timerertim/downlomatic/core/video/Video.kt
+++ shared/src/main/kotlin/eu/timerertim/downlomatic/core/video/Video.kt
@@ -3,4 +3,8 @@ package eu.timerertim.downlomatic.core.video
 import eu.timerertim.downlomatic.core.meta.VideoDetails
 import java.net.URL
 
+/**
+ * This represents a single, downloadable video saved in the application. It contains all information needed
+ * to identify a video, process and download it.
+ */
 data class Video(val url: URL, val details: VideoDetails, val metadata: Metadata)
