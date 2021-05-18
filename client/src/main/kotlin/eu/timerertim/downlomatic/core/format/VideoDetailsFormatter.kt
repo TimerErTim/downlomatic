ceae6257d592b3c607d9d1e5f4629b3a21af3776
TimerErTim <timerertim@gmail.com>
2021-05-18 22:19:10 +0200

    Reimplement working VideoDetailsFormatter

--- client/src/main/kotlin/eu/timerertim/downlomatic/core/format/VideoDetailsFormatter.kt
+++ client/src/main/kotlin/eu/timerertim/downlomatic/core/format/VideoDetailsFormatter.kt
@@ -3,7 +3,6 @@ package eu.timerertim.downlomatic.core.format
 import eu.timerertim.downlomatic.core.meta.VideoDetails
 import java.io.File
 import java.util.*
-import java.util.function.Consumer
 import java.util.regex.Pattern
 import java.util.stream.Collectors
 import kotlin.reflect.KProperty1
@@ -14,145 +13,39 @@ import kotlin.reflect.KProperty1
  *
  * The formatting happens
  * through [VideoDetailsFormatter.format] which takes a [Metadata] object as input. Everything you need to know is
- * described in that methods JavaDoc documentation. The format template is assigned in the [VideoDetailsFormatter] constructor.
+ * described in that methods JavaDoc documentation. The format template is assigned in the [VideoDetailsFormatter]
+ * constructor.
  */
-class VideoDetailsFormatter(private val pattern: String) {
+class VideoDetailsFormatter(pattern: String) {
+    private val contentList = pattern.splitAsSegment(0, 0)
+    private lateinit var identifiers: List<Replaceable>
+
     /**
      * Formats the episodes description to a readable `String` using the given expression.
      *
      *
      * ** Important to read the documentation under the following link:  [WIKI_URL]**
      *
-     * @param expression a String expression used as template for formatting
+     * @param  videoDetails the `VideoDetails` to be formatted
      * @return a formatted String representation of an episode
      */
     fun format(videoDetails: VideoDetails): String {
-        val identifiers = mutableListOf(*staticIdentifiers.toTypedArray()).also {
-            for (template in templateIdentifiers) {
+        identifiers = mutableListOf(*staticIdentifiers.toTypedArray()).also {
+            for (template in dynamicIdentifiers) {
                 it.add(template[videoDetails])
             }
-        }.toList()
+        }.sortedBy(Replaceable::priority)
 
-        var subSegments: List<String>
-        var result: String = pattern
-
-        // If the expression is a segment
-        if (findSegments(result).also { subSegments = it }.size == 1 && subSegments[0] == result) {
-            result = result.substring(2, result.length - 2)
-            subSegments = findSegments(result)
-
-            // Remove inner segments
-            val replacements: MutableList<Literal> = LinkedList()
-            for (s in subSegments) {
-                replacements.add(Illegal(s))
-            }
-            val temp = replaceSimultaneous(result, replacements)
-
-            // Return empty string according to segment logic
-            for (identifier in identifiers) {
-                val hasValue = identifier.hasValue()
-                if (temp.contains(identifier.identifier) && !hasValue || temp.contains(identifier.negativeIdentifier) && hasValue) {
-                    return ""
-                }
-            }
-        }
-
-        // Calculate replacements for inner segments
-        val replacements: MutableList<Literal> = LinkedList()
-        for (s in subSegments) {
-            replacements.add(Literal(s, VideoDetailsFormatter(s).format(videoDetails)))
-        }
-
-        // Return filled in expression
-        return replaceSimultaneous(result, replacements, identifiers)
-    }
-
-    private fun replaceSimultaneous(
-        input: String,
-        extra: List<Identifier>,
-        baseIdentifiers: List<Identifier> = listOf()
-    ): String {
-        // Create replacement pool
-        val identifiers: MutableList<Identifier> = LinkedList(extra)
-        identifiers.addAll(baseIdentifiers)
-
-        // Create replacement regular expression
-        val regexp = identifiers.stream().map { obj: Identifier -> obj.replaceRegex }
-            .collect(Collectors.joining("|"))
-        return if (regexp.isEmpty()) {
-            // Return input as is if there's nothing to replace
-            input
-        } else {
-            // Create replacement map out of replacement pool
-            val replacements: MutableMap<String, String> = HashMap()
-            identifiers.forEach(Consumer { identifier: Identifier ->
-                replacements[identifier.identifier] = identifier.getValue()
-                replacements[identifier.negativeIdentifier] = ""
-            })
-
-            // Replace everything "simultaneously"
-            val sb = StringBuffer()
-            val p = Pattern.compile(regexp)
-            val m = p.matcher(input)
-            while (m.find()) {
-                m.appendReplacement(sb, replacements[m.group()])
-            }
-            m.appendTail(sb)
-            sb.toString()
-        }
-    }
-
-    private open class Identifier(
-        val identifier: String,
-        val negativeIdentifier: String,
-        private val value: String?
-    ) {
-        constructor(key: String, value: String?) : this("/$key", "/!$key",
-            value?.replace(
-                staticIdentifiers
-                    .filterIsInstance<Illegal>()
-                    .map { obj: Illegal -> obj.replaceRegex }
-                    .joinToString("|").toRegex(),
-                ""
-            ))
-
-        open fun hasValue(): Boolean {
-            return value != null
-        }
-
-        open val replaceRegex: String?
-            get() = "(" + Pattern.quote(identifier) + "|" +
-                    Pattern.quote(negativeIdentifier) + ")"
-
-        fun getValue(): String {
-            return value ?: ""
-        }
-    }
-
-    private open class Literal(key: String, value: String) :
-        Identifier(key, Character.MIN_VALUE.toString(), value) {
-        override fun hasValue(): Boolean {
-            return true
-        }
-
-        override val replaceRegex: String?
-            get() = "(" + Pattern.quote(identifier) + ")"
-    }
-
-    private class Illegal(key: String) : Literal(key, "")
-
-    private class IdentifierTemplate<T>(
-        val key: String,
-        val value: KProperty1<VideoDetails, T>,
-        val modifier: T.() -> Any? = { this }
-    ) {
-        operator fun get(details: VideoDetails) = Identifier(key, modifier(value.get(details))?.toString())
+        return contentList.joinToString("") { it.process() }
     }
 
     companion object {
         const val WIKI_URL = "https://github.com/TimerErTim/downlomatic/wiki/Formatting"
 
-        private val staticIdentifiers = listOf<Identifier>(
+        private const val openingBracket = "/["
+        private const val closingBracket = "/]"
+
+        private val staticIdentifiers = listOf(
             Literal("//", File.separator),
 
             Illegal("/"),
@@ -168,54 +61,210 @@ class VideoDetailsFormatter(private val pattern: String) {
             Illegal("|")
         )
 
-        private val templateIdentifiers = listOf(
+        private val dynamicIdentifiers = listOf(
             IdentifierTemplate("N", VideoDetails::title),
             IdentifierTemplate("S", VideoDetails::series),
             IdentifierTemplate("s", VideoDetails::season),
             IdentifierTemplate("e", VideoDetails::episode),
             IdentifierTemplate("L", VideoDetails::audienceLanguage),
-            IdentifierTemplate("D", VideoDetails::spokenLanguage),
-            IdentifierTemplate("S", VideoDetails::subtitleLanguage),
+            IdentifierTemplate("A", VideoDetails::spokenLanguage),
+            IdentifierTemplate("V", VideoDetails::subtitleLanguage),
             IdentifierTemplate("T", VideoDetails::translation),
             IdentifierTemplate("y", VideoDetails::release) {
-                this?.year
+                year
             }
         )
 
-        private fun findSegments(input: String): List<String> {
-            val segments: MutableList<String> = LinkedList()
-            val stack = Stack<String>()
-            var startIndex = 0
-            var endIndex: Int
-            var openingIndex: Int
-            var closingIndex: Int
-            var index = 0
-            // Checking for opening and closing brackets with a stack
-            while (true) {
-                openingIndex = input.indexOf("/[", index)
-                closingIndex = input.indexOf("/]", index)
-                if (openingIndex == -1 && closingIndex == -1) {
-                    break
-                } else if (openingIndex < closingIndex && openingIndex != -1) {
-                    if (stack.isEmpty()) {
-                        startIndex = openingIndex
-                    }
-                    stack.push("/[")
-                    index = openingIndex
-                } else if (closingIndex < openingIndex || openingIndex == -1) {
-                    if (!stack.isEmpty()) {
-                        stack.pop()
-                        if (stack.isEmpty()) {
-                            // If the stack is empty, the last bracket must have been reached, so we can save a segment
-                            endIndex = closingIndex + 1
-                            segments.add(input.substring(startIndex, endIndex + 1))
-                        }
-                    }
-                    index = closingIndex
-                }
-                index++
+    }
+
+    private sealed interface Processable {
+        /**
+         * Process this object to return a human readable [String].
+         */
+        fun process(): String
+    }
+
+    private inner class PatternPart(private val content: String) : Processable {
+        override fun process() = content.replaceIdentifiers()
+
+        fun checkIdentifiers() = content.checkForIdentifiers()
+
+        override fun toString() = content
+    }
+
+    private inner class Segment(private val content: String) : Processable {
+        private val contentList: List<Processable> = content.splitAsSegment()
+
+        override fun process(): String {
+            val patternPartContentList = contentList.filterIsInstance<PatternPart>()
+
+            // Return empty string according to segment logic
+            if (patternPartContentList.any { it.checkIdentifiers() }) {
+                return ""
             }
-            return segments
+
+            return contentList.joinToString("") { it.process() }
         }
+
+        override fun toString() = content
+
+        fun String.splitByContent() = split(content, ignoreCase = false, limit = 2)
+    }
+
+    private sealed class Replaceable(open val priority: Int) {
+        open val replaceRegex get() = Regex("(${Regex.escape(key)})")
+        abstract val key: String
+        protected abstract val _value: String?
+
+        val hasValue get() = _value != null
+        val value get() = _value ?: ""
+    }
+
+    private class Identifier(
+        override val key: String,
+        value: String?
+    ) : Replaceable(0) {
+        val identifier = "/$key"
+        val negativeIdentifier = "/!$key"
+
+        override val replaceRegex = generateReplaceRegex()
+
+        override val _value = value?.replace(
+            staticIdentifiers
+                .filterIsInstance<Illegal>().joinToString("|") { it.replaceRegex.pattern }.toRegex(),
+            ""
+        )
+
+        fun generateReplaceRegex() = Regex(
+            "(" + Pattern.quote(identifier) + "|" +
+                    Pattern.quote(negativeIdentifier) + ")"
+        )
+    }
+
+    private class Literal(override val key: String, override val _value: String) : Replaceable(1)
+
+    private class Illegal(override val key: String) : Replaceable(2) {
+        override val _value = ""
+    }
+
+    private class IdentifierTemplate<T : Any>(
+        val key: String,
+        val value: KProperty1<VideoDetails, T?>,
+        val modifier: T.() -> Any = { this }
+    ) {
+        operator fun get(details: VideoDetails) = Identifier(key, value.get(details)?.let { modifier(it).toString() })
+    }
+
+    private fun String.splitAsSegment(
+        startMargin: Int = openingBracket.length,
+        endMargin: Int = closingBracket.length
+    ): List<Processable> {
+        val mutableContentList = mutableListOf<Processable>()
+        val trimmedContent = this.substring(startMargin, this.length - endMargin)
+        val subSegments = trimmedContent.extractSegments().map {
+            Segment(it)
+        }
+
+        // Build complex content list
+        var trimmedContentSplit = listOf(trimmedContent)
+        for (subSegment in subSegments) {
+            trimmedContentSplit = with(subSegment) { trimmedContentSplit.last().splitByContent() }
+            trimmedContentSplit.first().takeIf { it.isNotEmpty() }?.let { mutableContentList.add(PatternPart(it)) }
+            mutableContentList.add(subSegment)
+        }
+        trimmedContentSplit.last().takeIf { it.isNotEmpty() }?.let { mutableContentList.add(PatternPart(it)) }
+
+        return mutableContentList.toList()
+    }
+
+    private fun String.extractSegments(): List<String> {
+        val segments: MutableList<String> = LinkedList()
+        val stack = Stack<String>()
+        var startIndex = 0
+        var endIndex: Int
+        var openingIndex: Int
+        var closingIndex: Int
+        var index = 0
+        // Checking for opening and closing brackets with a stack
+        while (true) {
+            openingIndex = this.indexOf(openingBracket, index)
+            closingIndex = this.indexOf(closingBracket, index)
+            if (openingIndex == -1 && closingIndex == -1) {
+                break
+            } else if (openingIndex < closingIndex && openingIndex != -1) {
+                if (stack.isEmpty()) {
+                    startIndex = openingIndex
+                }
+                stack.push(openingBracket)
+                index = openingIndex
+            } else if (closingIndex < openingIndex || openingIndex == -1) {
+                if (!stack.isEmpty()) {
+                    stack.pop()
+                    if (stack.isEmpty()) {
+                        // If the stack is empty, the last bracket must have been reached, so we can save a segment
+                        endIndex = closingIndex + 1
+                        segments.add(this.substring(startIndex, endIndex + 1))
+                    }
+                }
+                index = closingIndex
+            }
+            index++
+        }
+        return segments
+    }
+
+    private fun String.replaceIdentifiers(): String {
+        // Create replacement regular expression
+        val regexp = identifiers.stream().map { it.replaceRegex.pattern }
+            .collect(Collectors.joining("|"))
+
+        // Create replacement map out of replacement pool
+        val replacements: MutableMap<String, String> = HashMap()
+        identifiers.forEach {
+            when (it) {
+                is Identifier -> {
+                    replacements[it.identifier] = it.value
+                    replacements[it.negativeIdentifier] = ""
+                }
+                else -> replacements[it.key] = it.value
+            }
+        }
+
+        // Replace everything "simultaneously"
+        val sb = StringBuffer()
+        val p = Pattern.compile(regexp)
+        val m = p.matcher(this)
+        while (m.find()) {
+            m.appendReplacement(sb, replacements[m.group()])
+        }
+        m.appendTail(sb)
+        return sb.toString()
+    }
+
+    private fun String.checkForIdentifiers(): Boolean {
+        // Create replacement regular expression
+        val regexp = identifiers.stream().map { it.replaceRegex.pattern }
+            .collect(Collectors.joining("|"))
+
+        // Shadow identifiers with a filtered list
+        val identifiers = identifiers.filterIsInstance<Identifier>()
+
+        // Create replacement map out of replacement pool
+        val identifierMap: MutableMap<String, Identifier> = HashMap()
+        val negativeIdentifierMap: MutableMap<String, Identifier> = HashMap()
+        identifiers.forEach {
+            identifierMap[it.identifier] = it
+            negativeIdentifierMap[it.negativeIdentifier] = it
+        }
+
+        // Check everything "simultaneously"
+        val p = Pattern.compile(regexp)
+        val m = p.matcher(this)
+        while (m.find()) {
+            if (identifierMap[m.group()]?.hasValue == false || negativeIdentifierMap[m.group()]?.hasValue == true) {
+                return true
+            }
+        }
+        return false
     }
 }
