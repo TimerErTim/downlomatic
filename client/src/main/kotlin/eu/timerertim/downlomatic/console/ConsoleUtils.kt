8ac9e84064237438600e8135666e835cb3570edd
TimerErTim <timerertim@gmail.com>
2021-05-18 16:28:26 +0200

    Add Formatter URL to ConsoleUtils

--- client/src/main/kotlin/eu/timerertim/downlomatic/console/ConsoleUtils.kt
+++ client/src/main/kotlin/eu/timerertim/downlomatic/console/ConsoleUtils.kt
@@ -1,5 +1,7 @@
 package eu.timerertim.downlomatic.console
 
+import eu.timerertim.downlomatic.core.format.VideoDetailsFormatter
+
 /**
  * Provides helpful methods for CLI and terminal interaction. This special implementation
  * handles the client program.
@@ -11,6 +13,6 @@ object ConsoleUtils : Console(
         "\"downlomatic -d <directory> -h <host> -a | -b <url> | -s <url> " +
                 "[-f <formatting>] [--subdir-format <formatting>] [-t <amount>]\"",
         "",
-        "Formatting follows the rules described under the following URL: "//${EpisodeFormat.WIKI_URL}"
+        "Formatting follows the rules described under the following URL: ${VideoDetailsFormatter.WIKI_URL}"
     )
 )
