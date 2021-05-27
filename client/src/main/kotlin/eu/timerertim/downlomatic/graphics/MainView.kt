263c6ed5268bea0233e0128086227a6454d225fe
TimerErTim <timerertim@gmail.com>
2021-05-27 13:55:21 +0200

    Add GUI functionality
    The GUI is empty for now, because this only adds the ability to have a GUI.

---
+++ client/src/main/kotlin/eu/timerertim/downlomatic/graphics/MainView.kt
@@ -0,0 +1,10 @@
+package eu.timerertim.downlomatic.graphics
+
+import tornadofx.View
+import tornadofx.borderpane
+
+class MainView : View("My View") {
+    override val root = borderpane {
+
+    }
+}
