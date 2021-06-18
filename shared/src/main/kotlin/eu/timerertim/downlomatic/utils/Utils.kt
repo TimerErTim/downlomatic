73d483c72dd7d303b00830f4f2bc90fcce819a3e
TimerErTim <timerertim@gmail.com>
2021-06-18 12:24:12 +0200

    Add toHumanReadableBytes extension functions
    This is needed for formatting later on in the GUI.

--- shared/src/main/kotlin/eu/timerertim/downlomatic/utils/Utils.kt
+++ shared/src/main/kotlin/eu/timerertim/downlomatic/utils/Utils.kt
@@ -1,6 +1,7 @@
 package eu.timerertim.downlomatic.utils
 
 import eu.timerertim.downlomatic.utils.logging.Log
+import kotlin.math.floor
 import kotlin.system.exitProcess
 
 /**
@@ -31,4 +32,60 @@ object Utils {
         // Exit with the given errorCode
         exitProcess(errorCode)
     }
+
+    /**
+     * Formats [this] number to a human readable amount of bytes. Units is MB-like.
+     */
+    @JvmStatic
+    fun Number.toHumanReadableBytesPowerOfTen(): String {
+        val bytes = this.toLong()
+
+        return when {
+            bytes == Long.MIN_VALUE || bytes < 0 -> "N/A"
+            bytes < 1000L -> "${bytes}B"
+            bytes <= 1000L pow 2 -> "%.1fKB".format((bytes.toDouble() / 1000L).floor(1))
+            bytes <= 1000L pow 3 -> "%.1fMB".format((bytes.toDouble() / (1000L pow 2)).floor(1))
+            bytes <= 1000L pow 4 -> "%.1fGB".format((bytes.toDouble() / (1000L pow 3)).floor(1))
+            bytes <= 1000L pow 5 -> "%.1fTB".format((bytes.toDouble() / (1000L pow 4)).floor(1))
+            bytes <= 1000L pow 6 -> "%.1fPB".format((bytes.toDouble() / (1000L pow 5)).floor(1))
+            else -> "%.1fEB".format((bytes.toDouble() / (1000L pow 6)).floor(1))
+        }
+    }
+
+    /**
+     * Formats [this] number to a human readable amount of bytes. Units is MiB-like.
+     */
+    @JvmStatic
+    fun Number.toHumanReadableBytesPowerOfTwo(): String {
+        val bytes = this.toLong()
+
+        return when {
+            bytes == Long.MIN_VALUE || bytes < 0 -> "N/A"
+            bytes < 1024L -> "${bytes}B"
+            bytes <= 0xfffccccccccccccL shr 40 -> "%.1fKiB".format(bytes.toDouble() / (0x1 shl 10))
+            bytes <= 0xfffccccccccccccL shr 30 -> "%.1fMiB".format(bytes.toDouble() / (0x1 shl 20))
+            bytes <= 0xfffccccccccccccL shr 20 -> "%.1fGiB".format(bytes.toDouble() / (0x1 shl 30))
+            bytes <= 0xfffccccccccccccL shr 10 -> "%.1fTiB".format(bytes.toDouble() / (0x1 shl 40))
+            bytes <= 0xfffccccccccccccL -> "%.1fPiB".format((bytes shr 10).toDouble() / (0x1 shl 40))
+            else -> "%.1fEiB".format((bytes shr 20).toDouble() / (0x1 shl 40))
+        }
+    }
+
+    /**
+     * Multiplies [this] Long [exponent] times with itself.
+     */
+    @JvmStatic
+    infix fun Long.pow(exponent: Int): Long {
+        var value = 1L
+        repeat(exponent) {
+            value *= this
+        }
+        return value
+    }
+
+    /**
+     * Calculates the floor of [this] to the given number of [digit].
+     */
+    @JvmStatic
+    fun Double.floor(digit: Int) = floor(this * (10L pow digit)) / (10L pow digit)
 }
