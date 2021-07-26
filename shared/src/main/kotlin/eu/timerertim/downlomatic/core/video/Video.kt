2571c2709b2c3dbffa7ec556b5bd379433d33224
TimerErTim <timerertim@gmail.com>
2021-07-26 02:47:22 +0200

    Implement REST API with Ktor

--- shared/src/main/kotlin/eu/timerertim/downlomatic/core/video/Video.kt
+++ shared/src/main/kotlin/eu/timerertim/downlomatic/core/video/Video.kt
@@ -8,4 +8,6 @@ import java.net.URL
  * This represents a single, downloadable video saved in the application. It contains all information needed
  * to identify a video, process and download it.
  */
-data class Video(val url: URL, val details: VideoDetails, val metadata: Metadata)
+data class Video(val url: URL, val details: VideoDetails, val metadata: Metadata) {
+    private val _id = details.hashCode()
+}
