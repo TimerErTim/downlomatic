28264572d6788d19c6f5a953cc39dcb58b5817ad
TimerErTim <timerertim@gmail.com>
2021-12-30 16:13:27 +0100

    Update Kotlin and dependencies version

--- client/build.gradle.kts
+++ client/build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
     kotlin("jvm")
-    id("org.jetbrains.compose") version "1.0.0"
+    id("org.jetbrains.compose") version "1.0.1"
 }
 
 repositories {
