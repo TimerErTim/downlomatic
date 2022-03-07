86b5feabfac0a7f246b81ee787750c120c5787e0
TimerErTim <timerertim@gmail.com>
2022-03-07 15:55:34 +0100

    Make api more robust

--- client/src/main/kotlin/eu/timerertim/downlomatic/api/KtorClient.kt
+++ client/src/main/kotlin/eu/timerertim/downlomatic/api/KtorClient.kt
@@ -3,12 +3,21 @@ package eu.timerertim.downlomatic.api
 import eu.timerertim.downlomatic.util.Utils
 import io.ktor.client.*
 import io.ktor.client.features.*
-import io.ktor.client.features.json.*
+import io.ktor.client.features.json.JsonFeature
+import io.ktor.client.features.json.serializer.*
 import io.ktor.client.request.*
+import kotlinx.serialization.json.Json
 
 val ktorClientLazy = lazy {
     HttpClient {
-        install(JsonFeature)
+        install(JsonFeature) {
+            serializer = KotlinxSerializer(
+                Json {
+                    isLenient = true
+                    ignoreUnknownKeys = true
+                }
+            )
+        }
         install(DefaultRequest) {
             this.host = KtorClient.server
             this.port = KtorClient.serverPort
